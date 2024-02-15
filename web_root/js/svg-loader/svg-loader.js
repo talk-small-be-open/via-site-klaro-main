3318c75bf9357e38f03c9b29838b249637c2b53d
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2024-02-15 16:14:24 +0100

    our own svg loader simplified

---
+++ web_root/js/svg-loader/svg-loader.js
@@ -0,0 +1,47 @@
+"use strict";
+
+async function svgLoadInline(elemId, callback) {
+	const elem = document.getElementById(elemId);
+  const url = new URL(elem.getAttribute("data-svgloader-src"), globalThis.location);
+
+	function renderBody(elem, body) {
+
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(body, "text/html");
+		const fragment = doc.querySelector("svg");
+
+		// Copy the inner SVG
+		elem.innerHTML = fragment.innerHTML;
+
+		// Copy the attributes on top level, no override
+		for (let i = 0; i < fragment.attributes.length; i++) {
+			const {
+				name,
+				value
+			} = fragment.attributes[i];
+
+			if (!elem.hasAttribute(name)) {
+				elem.setAttribute(name, value);
+			}
+		}
+
+		// Finished loading. Callback to some code from the app.
+		callback();
+		
+	};
+
+	// Start the fetching of the file
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(`Request for '${url.toString()}' returned ${response.status} (${response.statusText})`);
+      }
+      return response.text();
+    })
+    .then((body) => {
+      renderBody(elem, body);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
