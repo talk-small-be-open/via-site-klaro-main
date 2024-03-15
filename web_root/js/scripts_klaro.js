cb5be7c2f8f36754f0f17db6fe2c488325afb886
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2024-03-16 00:14:03 +0100

    progress

--- web_root/js/scripts_klaro.js
+++ web_root/js/scripts_klaro.js
@@ -42,13 +42,17 @@ function processHtmlDocument(contextElement) {
 
 // provided by seaside: function pager_getPager()
 
-function pager_gotoPage(pagerId, pageId) {
+function pager_gotoPage(pagerId, pageId, isBack = false) {
 	processHtmlDocument(pageId);
-	$('div.klaroPage', pagerId).removeClass('current appear');
+	window.scroll({behavior: 'auto', top: 0});
+
+	const appearanceStyle = isBack ? 'appearBack' : 'appear';
+	
+	$('div.klaroPage', pagerId).removeClass('current appear appearBack');
 
 	// Slide-In Animation! See CSS ...
 	
-	$(pageId).addClass('current appear');
+	$(pageId).addClass('current ' + appearanceStyle);
 }
 
 
@@ -57,39 +61,42 @@ function pager_gotoPage(pagerId, pageId) {
 
 class SpeedReaderTimer {
 	milliseconds = 0;
-	milliseconds_togo = 0;
+	millisecondsTogo = 0;
 	step = 100;
 	interval;
-	finished_callback;
+	finishedCallback;
+	syncCallback;
 	gameBoard;
 	animeAnimation;
-	speedFactor = 1;
+//	speedFactor = 1;
+	isStateRunning = null; // must be NOT set
 
 	// constructor() {
 	// }
 
 
-	init(gameBoardId, seconds, finishedCallback) {
+	init(gameBoardId, milliseconds, finishedCallback, syncCallback) {
 		this.gameBoard = $('#'+gameBoardId);
-		this.milliseconds = seconds * 1000 * this.speedFactor;
-		this.milliseconds_togo = this.milliseconds;
-		this.finished_callback = finishedCallback;
+		this.milliseconds = milliseconds;
+		this.millisecondsTogo = this.milliseconds;
+		this.finishedCallback = finishedCallback;
+		this.syncCallback = syncCallback;
 		this.redraw();
-		this.be_speed_normal();
+//		this.beSpeedNormal();
 	}
 
 
-	be_speed_normal() {
-		this.speedFactor = 1;
-	}
+	// beSpeedNormal() {
+	// 	this.speedFactor = 1;
+	// }
 
-	be_speed_fast() {
-		this.speedFactor = 0.75;
-	}
+	// beSpeedFast() {
+	// 	this.speedFactor = 0.75;
+	// }
 
-	be_speed_slow() {
-		this.speedFactor = 2;
-	}
+	// beSpeedSlow() {
+	// 	this.speedFactor = 2;
+	// }
 
 	redraw() {
 		//	var completness = this.milliseconds_togo / this.milliseconds;
@@ -97,7 +104,15 @@ class SpeedReaderTimer {
 		// $("span.mainButton.active svg.sign path.timerArc").attr('d', describeArc(35, 35, 32, 360 * completness))
 	}
 
+	syncWithServer() {
+		var data = this.isRunning();
+		this.syncCallback(data);
+	}
+	
 	pause() {
+		this.isStateRunning = false;
+		this.syncWithServer();
+
 		clearInterval(this.interval);
 		this.interval = null;
 		this.gameBoard.removeClass('playing').addClass('pausing');
@@ -105,17 +120,26 @@ class SpeedReaderTimer {
 	}
 
 	continue() {
-		this.interval = setInterval( () => { this.interval_ticker() }, this.step);
+		this.isStateRunning = true;
+		this.syncWithServer();
+
+		this.interval = setInterval( () => { this.intervalTicker() }, this.step);
 		this.gameBoard.removeClass('pausing').addClass('playing');
 		if (this.animeAnimation) { this.animeAnimation.play() }
 	}
 
-	is_running() {
-		return this.interval != null;
+	isRunning() {
+		return this.isStateRunning === true
+//		return this.interval != null;
 	}
 
+	isPaused() {
+		return this.isStateRunning === false
+	}
+
+	
 	toggle() {
-		if (this.is_running()) {
+		if (this.isRunning()) {
 			this.pause();
 		} else {
 			this.continue();
@@ -131,7 +155,7 @@ class SpeedReaderTimer {
 	finished() {
 		clearInterval(this.interval);
 		this.redraw();
-		this.finished_callback();
+		this.finishedCallback();
 		if (this.animeAnimation) { this.animeAnimation.pause() }
 
 	}
@@ -140,13 +164,23 @@ class SpeedReaderTimer {
 		this.continue();
 	}
 
-	load_animation(animeAnimation) {
+	stop() {
+		if (this.isRunning()) {
+			this.pause();
+		}
+	}
+
+	autostart() {
+		if (!this.isPaused()) { this.start() }
+	}
+
+	loadAnimation(animeAnimation) {
 		this.animeAnimation = animeAnimation;
 	}
 
-	interval_ticker() {
-		this.milliseconds_togo = this.milliseconds_togo - this.step;
-		if (this.milliseconds_togo <= 0) {
+	intervalTicker() {
+		this.millisecondsTogo = this.millisecondsTogo - this.step;
+		if (this.millisecondsTogo <= 0) {
 			this.finished();
 		} else {
 			this.redraw();
@@ -237,8 +271,22 @@ function enableAudio () {
 // }
 
 
+// ================ UI Events ====================
 
-/* Haupt JS init */
+// Event Handler for passive side elements, which will trigger the default button
+// For convenience in UI
+function catchPointerAndRetriggerDefaultButton(event) {
+	const t = $(event.target);
+	if ( t.hasClass('triggerDefaultButton') ||  t.parentsUntil(event.currentTarget).hasClass('triggerDefaultButton') ) {
+		// console.log(event);
+		$('.gameDefaultButton', event.currentTarget).trigger(event.type);
+	}
+}
+
+
+
+
+// ========= Haupt JS init ==============
 $(document).ready(function(){
 
 	processHtmlDocument(document);
